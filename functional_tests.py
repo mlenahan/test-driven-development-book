80997dde98549f0f1a8fa361e190647db7ca8b53
m_lenahan <michael_lenahan@hotmail.com>
2021-04-30 13:50:52 +0100

    front page html now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -3,16 +3,16 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-
 class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDown(self) -> None:
+    def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
@@ -20,9 +20,12 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'),
-                         'enter a to-do item'
-                         )
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
@@ -30,12 +33,10 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very
-        # methodical)
         self.fail('Finish the test!')
 
 
